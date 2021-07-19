c97b5b001a15fc9335b5671775b75b153e17c63a
bhrigun <bhrigun@gmail.com>
2021-07-19 12:16:37 +0530

    user1 changed to ubuntu

---
+++ remote2.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+apt update
+####tasksel install ubuntu-desktop
+apt --reinstall install gnome-terminal
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+apt -y install  /home/ubuntu/google-chrome-stable_current_amd64.deb /home/ubuntu/chrome-remote-desktop_current_amd64.deb
+
+cat <<EOF >/etc/default/apport
+# set this to 0 to disable apport, or to 1 to enable it
+# you can temporarily override this with
+# sudo service apport start force_start=1
+enabled=0
+EOF
+
+cat <<EOF >/etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
+[Allow Colord all Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+cat <<EOF >/etc/polkit-1/localauthority/50-local.d/46-allow-update-repo.pkl
+[Allow Package Management all Users]
+Identity=unix-user:*
+Action=org.freedesktop.packagekit.system-sources-refresh
+ResultAny=yes
+ResultInactive=yes
+ResultActive=yes
+EOF
+
+cat <<EOF >/etc/gdm3/custom.conf
+# GDM configuration storage
+#
+# See /usr/share/gdm/gdm.schemas for a list of available options.
+
+[daemon]
+# Uncoment the line below to force the login screen to use Xorg
+#WaylandEnable=false
+
+# Enabling automatic login
+  AutomaticLoginEnable = true
+  AutomaticLogin = ubuntu
+
+# Enabling timed login
+#  TimedLoginEnable = true
+#  TimedLogin = user1
+#  TimedLoginDelay = 10
+
+[security]
+
+[xdmcp]
+
+[chooser]
+
+[debug]
+# Uncomment the line below to turn on debugging
+# More verbose logs
+# Additionally lets the X server dump core if it crashes
+#Enable=true
+EOF
+
+su ubuntu -l -c "gsettings set org.gnome.desktop.screensaver lock-enabled false"
+su ubuntu -l -c "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'"
+# -l --login is crucial, it login as user1 and -c lets execute command 
+#apt -y install language-pack-ja libreoffice-help-ja gnome-user-docs-ja language-pack-gnome-ja thunderbird-locale-ja mozc-utils-gui firefox-locale-ja ibus-mozc fonts-noto-cjk-extra libreoffice-l10n-ja gnome-getting-started-docs-ja
+
+apt -y install language-pack-ja libreoffice-help-ja gnome-user-docs-ja language-pack-gnome-ja thunderbird-locale-ja mozc-utils-gui firefox-locale-ja ibus-mozc fonts-noto-cjk-extra libreoffice-l10n-ja gnome-getting-started-docs-ja hunspell-en-ca thunderbird-locale-en-us wbritish libreoffice-l10n-en-gb mythes-en-au language-pack-gnome-en libreoffice-help-en-us hyphen-en-gb hyphen-en-us hunspell-en-za mythes-en-us hyphen-en-ca hunspell-en-au libreoffice-help-en-gb language-pack-en hunspell-en-gb thunderbird-locale-en-gb firefox-locale-en libreoffice-l10n-en-za thunderbird-locale-en
+
