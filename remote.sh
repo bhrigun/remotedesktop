216a20ea3bde94fb3ab5b208df5e248f8b05e972
bhrigun <bhrigun@gmail.com>
2022-02-10 09:25:34 +0530

    updated remote2

--- remote.sh
+++ remote.sh
@@ -31,37 +31,6 @@ ResultInactive=yes
 ResultActive=yes
 EOF
 
-cat <<EOF >/etc/gdm3/custom.conf
-# GDM configuration storage
-#
-# See /usr/share/gdm/gdm.schemas for a list of available options.
-
-[daemon]
-# Uncoment the line below to force the login screen to use Xorg
-#WaylandEnable=false
-
-# Enabling automatic login
-  AutomaticLoginEnable = true
-  AutomaticLogin = user1
-
-# Enabling timed login
-#  TimedLoginEnable = true
-#  TimedLogin = user1
-#  TimedLoginDelay = 10
-
-[security]
-
-[xdmcp]
-
-[chooser]
-
-[debug]
-# Uncomment the line below to turn on debugging
-# More verbose logs
-# Additionally lets the X server dump core if it crashes
-#Enable=true
-EOF
-
 su user1 -l -c "gsettings set org.gnome.desktop.screensaver lock-enabled false"
 su user1 -l -c "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'"
 # -l --login is crucial, it login as user1 and -c lets execute command 
